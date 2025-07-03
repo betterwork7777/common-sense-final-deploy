92f59018facb0a8d0396fe59bdf4732c6c9eca16
betterwork7777 <kiontroy@gmail.com>
2025-07-02 18:40:26 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Common Sense Doc Services</h1>
+      <p>Slogan: Affordable Help for Everyday People</p>
+      <p>Phone: 425-584-8159 | Email: info.commonsense.com</p>
+      <p>Languages: English, Spanish, Korean</p>
+    </div>
+  );
+}
+
+export default App;
